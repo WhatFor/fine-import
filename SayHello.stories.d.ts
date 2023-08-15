0a4fe29f676a05de61070c78d01925fd1edb9bec
WhatFor <3914800+WhatFor@users.noreply.github.com>
2023-08-15 20:39:14 +0000

    Deploying to gh-pages from  @ 5e59c3187466e874eb91e8044cd309d0d14908f5 🚀

---
+++ SayHello.stories.d.ts
@@ -0,0 +1,6 @@
+import { Meta, StoryObj } from "@storybook/react";
+import SayHello from ".";
+declare const meta: Meta<typeof SayHello>;
+export default meta;
+type Story = StoryObj<typeof SayHello>;
+export declare const Default: Story;
